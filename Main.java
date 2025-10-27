b0132b73414102d6108d4cf8150e40f52a569b25
Leleh Nobre <lethicianobre2@gmail.com>
2025-10-27 20:35:53 -0300

    vertice

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-    System.out.println("HELLO WORD");
+    System.out.println("As vertices são os objetos\nAs arestas são as ligações entre elas");
     }
 }
